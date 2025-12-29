60bb199896229318ceeeacd36112503a1be59587
Jinqi Xiao <jinqi.xiao@rutgers.edu>
2025-12-28 20:45:49 -0800

    update

--- eval.sh
+++ eval.sh
@@ -1,16 +1,12 @@
-for dir in output/moe/*/checkpoint-20000; do
+for dir in output/moe/*/checkpoint; do
     if [ -d "$dir" ]; then
         echo "$dir"
         temp_path="${dir#output/moe/}"
-        moe_dir_name="${temp_path%/checkpoint-20000}"
+        moe_dir_name="${temp_path%/checkpoint}"
         echo "$moe_dir_name"
-        # bash eval_edit.sh $dir "$moe_dir_name"_false
         bash eval_edit.sh $dir "$moe_dir_name"
         echo "------------------------------------"
     fi
 done
 
-bash eval_edit.sh output/moe/typeD_tokenS_new_49k typeD_tokenS_new_49k
-bash eval_edit.sh output/moe/typeD_tokenS_old_69k typeD_tokenS_old_69k
-cd ..
-python gpu_occupy.py 
+bash eval_edit.sh your_model weights
